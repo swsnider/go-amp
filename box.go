b4fc15c12f116a6308c138c1a4af2f6c1683150b
Silas Snider <swsnider@gmail.com>
2014-05-22 21:28:02 -0700

    correct behaviour to ensure that boxes always output _ask and _command first

--- box.go
+++ box.go
@@ -1,88 +1,119 @@
 package amp
 
 import (
-	"bytes"
-	"encoding/binary"
-	"fmt"
-	"io"
-	"unicode/utf8"
+  "bytes"
+  "encoding/binary"
+  "fmt"
+  "io"
+  "unicode/utf8"
 )
 
 type Box map[string]string
 
 const (
-	MAX_KEY_LENGTH = 255
-	MAX_VAL_LENGTH = 65535
+  MAX_KEY_LENGTH = 255
+  MAX_VAL_LENGTH = 65535
 )
+
+func writeKV(buf *bytes.Buffer, k string, v string) error {
+  if err := binary.Write(buf, binary.BigEndian, uint16(len(k))); err != nil {
+    return fmt.Errorf("unable to write length for key %q: %v", k, err)
+  }
+  if err := binary.Write(buf, binary.BigEndian, []byte(k)); err != nil {
+    return fmt.Errorf("unable to write key %q: %v", k, err)
+  }
+  if err := binary.Write(buf, binary.BigEndian, uint16(len(v))); err != nil {
+    return fmt.Errorf("unable to write length of value for key %q: %v", k, err)
+  }
+  if err := binary.Write(buf, binary.BigEndian, []byte(v)); err != nil {
+    return fmt.Errorf("unable to write value for key %q: %v", k, err)
+  }
+  return nil
+}
 
 // Serialize converts an AMP Box to the wire representation, if possible.
 func (b Box) Serialize() ([]byte, error) {
-	buf := new(bytes.Buffer)
-	for k, v := range b {
-		if !utf8.ValidString(k) {
-			return nil, fmt.Errorf("key %q is not valid utf8", k)
-		}
-		if !utf8.ValidString(v) {
-			return nil, fmt.Errorf("value %q is not valid utf8", k)
-		}
-		if len(k) > MAX_KEY_LENGTH {
-			return nil, fmt.Errorf("key %q has length %v which is longer than max length %q", k, len(k), MAX_KEY_LENGTH)
-		}
-		if len(v) > MAX_VAL_LENGTH {
-			return nil, fmt.Errorf("value %q has length %v which is longer than max length %q", v, len(v), MAX_VAL_LENGTH)
-		}
-
-		binary.Write(buf, binary.BigEndian, uint16(len(k)))
-		binary.Write(buf, binary.BigEndian, []byte(k))
-		binary.Write(buf, binary.BigEndian, uint16(len(v)))
-		binary.Write(buf, binary.BigEndian, []byte(v))
-	}
-	buf.WriteString("\x00\x00")
-	return buf.Bytes(), nil
+  var err error
+  buf := new(bytes.Buffer)
+  ask, askok := b["_ask"]
+  cmd, cmdok := b["_command"]
+  if !cmdok {
+    return nil, fmt.Errorf("_command key is not present, despite being required.")
+  }
+  if askok {
+    err = writeKV(buf, "_ask", ask)
+    if err != nil {
+      return nil, err
+    }
+  }
+  err = writeKV(buf, "_command", cmd)
+  if err != nil {
+    return nil, err
+  }
+  for k, v := range b {
+    if !utf8.ValidString(k) {
+      return nil, fmt.Errorf("key %q is not valid utf8", k)
+    }
+    if !utf8.ValidString(v) {
+      return nil, fmt.Errorf("value %q is not valid utf8", k)
+    }
+    if len(k) > MAX_KEY_LENGTH {
+      return nil, fmt.Errorf("key %q has length %v which is longer than max length %q", k, len(k), MAX_KEY_LENGTH)
+    }
+    if len(v) > MAX_VAL_LENGTH {
+      return nil, fmt.Errorf("value %q has length %v which is longer than max length %q", v, len(v), MAX_VAL_LENGTH)
+    }
+    err = writeKV(buf, k, v)
+    if err != nil {
+      return nil, err
+    }
+  }
+  buf.WriteString("\x00\x00")
+  return buf.Bytes(), nil
 }
 
 // Deserialize converts a wire representation to an AMP box, if possible.
 func Deserialize(b []byte) (Box, error) {
-	if len(b) < 2 {
-		return nil, fmt.Errorf("serialized AMP box length was %v, shorter than two bytes, which is illegal", len(b))
-	}
-	return Decode(bytes.NewBuffer(b))
+  if len(b) < 2 {
+    return nil, fmt.Errorf("serialized AMP box length was %v, shorter than two bytes, which is illegal", len(b))
+  }
+  return Decode(bytes.NewBuffer(b))
 }
 
 func Decode(buf io.Reader) (Box, error) {
-	box := make(Box)
-	for {
-		var l uint16
-		err := binary.Read(buf, binary.BigEndian, &l)
-		if err != nil {
-			return nil, fmt.Errorf("unable to read length of key from buffer: %v", err)
-		}
-		if l == 0 {
-			break
-		}
-		kbytes := make([]byte, l)
-		err = binary.Read(buf, binary.BigEndian, kbytes)
-		if err != nil {
-			return nil, fmt.Errorf("unable to read key of length %v from buffer: %v", l, err)
-		}
-		if !utf8.Valid(kbytes) {
-			return nil, fmt.Errorf("key %q is an invalid utf-8 string", kbytes)
-		}
-		k := string(kbytes)
+  box := make(Box)
+  for {
+    var l uint16
+    err := binary.Read(buf, binary.BigEndian, &l)
+    if err != nil {
+      return nil, fmt.Errorf("unable to read length of key from buffer: %v", err)
+    }
+    if l == 0 {
+      break
+    }
+    kbytes := make([]byte, l)
+    err = binary.Read(buf, binary.BigEndian, kbytes)
+    if err != nil {
+      return nil, fmt.Errorf("unable to read key of length %v from buffer: %v", l, err)
+    }
+    if !utf8.Valid(kbytes) {
+      return nil, fmt.Errorf("key %q is an invalid utf-8 string", kbytes)
+    }
+    k := string(kbytes)
 
-		err = binary.Read(buf, binary.BigEndian, &l)
-		if err != nil {
-			return nil, fmt.Errorf("unable to read length of value from buffer: %v", err)
-		}
-		vbytes := make([]byte, l)
-		err = binary.Read(buf, binary.BigEndian, vbytes)
-		if err != nil {
-			return nil, fmt.Errorf("unable to read value of length %v from buffer: %v", l, err)
-		}
-		if !utf8.Valid(vbytes) {
-			return nil, fmt.Errorf("value %q is an invalid utf-8 string", vbytes)
-		}
-		box[k] = string(vbytes)
-	}
-	return box, nil
+    err = binary.Read(buf, binary.BigEndian, &l)
+    if err != nil {
+      return nil, fmt.Errorf("unable to read length of value from buffer: %v", err)
+    }
+    vbytes := make([]byte, l)
+    err = binary.Read(buf, binary.BigEndian, vbytes)
+    if err != nil {
+      return nil, fmt.Errorf("unable to read value of length %v from buffer: %v", l, err)
+    }
+    if !utf8.Valid(vbytes) {
+      return nil, fmt.Errorf("value %q is an invalid utf-8 string", vbytes)
+    }
+    box[k] = string(vbytes)
+  }
+  return box, nil
 }
--- box_test.go
+++ box_test.go
@@ -1,30 +1,28 @@
 package amp
 
 import (
-	"reflect"
-	"testing"
+  "reflect"
+  "testing"
 )
 
 func TestSerializationRoundTrip(t *testing.T) {
-	box := Box{
-		"_ask":     "1",
-		"_command": "listpeer",
-		"payload":  "{\"key\": \"value\"}",
-	}
-	bytes, err := box.Serialize()
-	if err != nil {
-		t.Errorf("box.Serialize() returned error: %v", err)
-		t.FailNow()
-	}
-	t.Logf("%v", string(bytes))
-	got, err := Deserialize(bytes)
-	if err != nil {
-		t.Errorf("Deserialize() returned error: %v", err)
-		t.FailNow()
-	}
-	if !reflect.DeepEqual(got, box) {
-		t.Errorf("Got %+v, but was expecting %+v.", got, box)
-	}
+  box := Box{
+    "_ask":     "1",
+    "_command": "listpeer",
+    "payload":  "{\"key\": \"value\"}",
+  }
+  bytes, err := box.Serialize()
+  if err != nil {
+    t.Fatalf("box.Serialize() returned error: %v", err)
+  }
+  t.Logf("%v", string(bytes))
+  got, err := Deserialize(bytes)
+  if err != nil {
+    t.Fatalf("Deserialize() returned error: %v", err)
+  }
+  if !reflect.DeepEqual(got, box) {
+    t.Fatalf("Got %+v, but was expecting %+v.", got, box)
+  }
 }
 
 const pythonGeneratedWire = "\x00\x04_ask\x00\x011\x00\x08_command\x00\x08listpeer\x00\x00"
@@ -32,12 +30,24 @@ const pythonGeneratedWire = "\x00\x04_ask\x00\x011\x00\x08_command\x00\x08listpe
 var pythonGeneratedBox = Box{"_ask": "1", "_command": "listpeer"}
 
 func TestDeserializeFromPython(t *testing.T) {
-	got, err := Deserialize([]byte(pythonGeneratedWire))
-	if err != nil {
-		t.Errorf("Deserialize() returned error: %v", err)
-		t.FailNow()
-	}
-	if !reflect.DeepEqual(got, pythonGeneratedBox) {
-		t.Errorf("Got %+v, but was expecting %+v.", got, pythonGeneratedBox)
-	}
+  got, err := Deserialize([]byte(pythonGeneratedWire))
+  if err != nil {
+    t.Fatalf("Deserialize() returned error: %v", err)
+  }
+  if !reflect.DeepEqual(got, pythonGeneratedBox) {
+    t.Fatalf("Got %+v, but was expecting %+v.", got, pythonGeneratedBox)
+  }
+}
+
+func TestSerializeOrder(t *testing.T) {
+  got, err := pythonGeneratedBox.Serialize()
+  if err != nil {
+    t.Fatal(err)
+  }
+  if string(got[2:6]) != "_ask" {
+    t.Fatal("_ask key is not the first key in the serialization.")
+  }
+  if string(got[11:19]) != "_command" {
+    t.Fatal("_command key is not the second key in the serialization.")
+  }
 }
